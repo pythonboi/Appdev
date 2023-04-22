89d5cb13a6637b74b04c5352fda7baf0e6104f7e
pythonboi <hafeeztukur@gmail.com>
2023-04-22 19:19:53 -0400

    update the model to be able to return string for the name and city

--- pizzaproject/stores/admin.py
+++ pizzaproject/stores/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pizzeria
+
+# This will register the Pizzeria model into the admin.py to abe able to see it in our admin page
+
+admin.site.register(Pizzeria)
